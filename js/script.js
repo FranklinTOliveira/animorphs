b2695e3584bb5765db5b42fb22d6a9c1a5fb65e8
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2022-01-21 13:03:33 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const taul = document.querySelector['taul'];
+const bul = document.querySelector['bul'];
+
+const pul = document.createElement('p');
+pul.innerHTML = 'hello world';
+document.body.appendChild();
+textarea.value = '';
+
+const taul = document.querySelector['taul'];
+const bul = document.querySelector['bul'];
+
+const pul = document.createElement('p');
+pul.innerHTML = 'hello world';
+document.body.appendChild();
+textarea.value = '';
+
+const taul = document.querySelector['taul'];
+const bul = document.querySelector['bul'];
+
+const pul = document.createElement('p');
+pul.innerHTML = 'hello world';
+document.body.appendChild();
+textarea.value = '';
+
+const taul = document.querySelector['taul'];
+const bul = document.querySelector['bul'];
+
+const pul = document.createElement('p');
+pul.innerHTML = 'hello world';
+document.body.appendChild();
+textarea.value = '';
+
+
+
+
