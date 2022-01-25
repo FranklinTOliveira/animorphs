d48779f0a563a127983b350b003152a95e4a0f09
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2022-01-24 20:35:12 -0800

    added event listeners

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 const taul = document.querySelector['taul'];
 const bul = document.querySelector['bul'];
 
-const pul = document.createElement('p');
-pul.innerHTML = 'hello world';
-document.body.appendChild();
+// bul.addEventListener("click", function); 
+
+const pul = document.body.appendChild();
 textarea.value = '';
 alert('Someone left a comment.');
 
 const taur = document.querySelector['taur'];
-taur.style.display;
+const showtaur = taur.style.display;
 
 const bur = document.querySelector['bur'];
+bur.addEventListener("click", showtaur);
 
-const plrl = document.getElementById('talrl');
-plrl.style.color = 'white';
+const talrl = document.getElementById("talrl");
+talrl.style.backgroundColor = 'black';
+
+function brighten(el) {
+  if (el.style.backgroundColor === 'black') {
+    el.style.backgroundColor = 'white';   
+  }
+  else el.style.backgroundColor = 'black';
+}
 
 const blrl = document.querySelector['blrl'];
+blrl.addEventListener("click", brighten);
